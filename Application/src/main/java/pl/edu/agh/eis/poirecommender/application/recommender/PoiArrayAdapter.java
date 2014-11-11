1d76fc56d9b751b9cf507b9c8800c1a0af12d4ae
Krzysztof Balon <k.bb@wp.pl>
2014-11-11 16:19:28 +0100

    SC# S-005 created PoiArrayAdapter using PoiManager

---
+++ Application/src/main/java/pl/edu/agh/eis/poirecommender/application/recommender/PoiArrayAdapter.java
@@ -0,0 +1,53 @@
+package pl.edu.agh.eis.poirecommender.application.recommender;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import pl.edu.agh.eis.poirecommender.R;
+import pl.edu.agh.eis.poirecommender.pois.PoiManager;
+import pl.edu.agh.eis.poirecommender.pois.model.PoiAtDistance;
+
+import java.util.List;
+
+/**
+ * Created by Krzysztof Balon on 2014-11-11.
+ */
+public class PoiArrayAdapter extends ArrayAdapter<PoiAtDistance> {
+    private static final String DISTANCE_UNIT = "km";
+    private final PoiManager poiManager;
+    private List<PoiAtDistance> poiList;
+
+    public PoiArrayAdapter(Context context) {
+        super(context, R.layout.poi_row);
+        this.poiManager = new PoiManager(context);
+        this.poiList = poiManager.getPoiList();
+    }
+
+    @Override
+    public void notifyDataSetChanged() {
+        this.poiList = poiManager.getPoiList();
+        super.notifyDataSetChanged();
+    }
+
+    @Override
+    public int getCount() {
+        return poiList.size();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.poi_row, parent, false);
+        }
+        PoiAtDistance poi = poiList.get(position);
+        final TextView poiNameText = (TextView) convertView.findViewById(R.id.poi_name);
+        final TextView poiDistanceText = (TextView) convertView.findViewById(R.id.poi_distance);
+        poiNameText.setText(poi.getName());
+        poiDistanceText.setText(String.format("%.1f%s", poi.getDistance(), DISTANCE_UNIT));
+        return convertView;
+    }
+}
