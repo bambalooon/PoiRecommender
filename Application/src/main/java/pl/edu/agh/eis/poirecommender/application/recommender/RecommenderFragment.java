9a10cd8745dc39cbbbb0e00a30a03f27a8072614
BamBalooon <k.bb@wp.pl>
2015-05-04 21:10:43 +0200

    RF# changed poiArrayAdapter field type to PoiArrayAdapter class, added onListItemClick method which shows PoiFragment when poi on the list was clicked

--- Application/src/main/java/pl/edu/agh/eis/poirecommender/application/recommender/RecommenderFragment.java
+++ Application/src/main/java/pl/edu/agh/eis/poirecommender/application/recommender/RecommenderFragment.java
@@ -3,12 +3,18 @@ package pl.edu.agh.eis.poirecommender.application.recommender;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.view.*;
-import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import pl.edu.agh.eis.poirecommender.R;
+import pl.edu.agh.eis.poirecommender.application.poi.PoiFragment;
 
-
+/**
+ * Name: RecommenderFragment
+ * Description: RecommenderFragment
+ * Date: 2014-11-11
+ * Created by BamBalooon
+ */
 public class RecommenderFragment extends ListFragment {
-    private ArrayAdapter<?> poiArrayAdapter;
+    private PoiArrayAdapter poiArrayAdapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -43,5 +49,14 @@ public class RecommenderFragment extends ListFragment {
     public void onResume() {
         super.onResume();
         poiArrayAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        PoiFragment poiFragment = PoiFragment.newInstance(poiArrayAdapter.getItem(position).getElement());
+        getActivity().getSupportFragmentManager().beginTransaction()
+                .replace(R.id.content_frame, poiFragment)
+                .addToBackStack(null)
+                .commit();
     }
 }
