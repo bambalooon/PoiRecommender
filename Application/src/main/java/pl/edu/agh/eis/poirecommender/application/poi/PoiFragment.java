e7a27c0af8ab973c8f9ffc1297590fefcf6c5331
BamBalooon <k.bb@wp.pl>
2015-05-09 19:37:28 +0200

    RF# switched to PoiRecommenderServiceInvoker for service invocation, PoiFragment now stores Poi instead of Element, created RatingProvider inner class that translates rating from RatingBar to 0-1 scale

--- Application/src/main/java/pl/edu/agh/eis/poirecommender/application/poi/PoiFragment.java
+++ Application/src/main/java/pl/edu/agh/eis/poirecommender/application/poi/PoiFragment.java
@@ -1,20 +1,20 @@
 package pl.edu.agh.eis.poirecommender.application.poi;
 
-import android.app.PendingIntent;
-import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RatingBar;
 import android.widget.TextView;
 import com.aware.poirecommender.openstreetmap.model.response.Element;
-import com.aware.poirecommender.service.PoiRecommenderService;
 import com.aware.poirecommender.transform.Serializer;
 import com.google.common.base.Preconditions;
 import pl.edu.agh.eis.poirecommender.R;
+import pl.edu.agh.eis.poirecommender.pois.model.OsmPoi;
+import pl.edu.agh.eis.poirecommender.pois.model.Poi;
 
 /**
  * Name: PoiFragment
@@ -23,7 +23,6 @@ import pl.edu.agh.eis.poirecommender.R;
  * Created by BamBalooon
  */
 public class PoiFragment extends Fragment {
-    private static final String TAG = PoiFragment.class.getSimpleName();
     private static final String ARGUMENT_POI_ELEMENT = "ARGUMENT_POI_ELEMENT";
     /**
      * TODO: move request code constant to specific class with global request codes for pending intents
@@ -32,7 +31,9 @@ public class PoiFragment extends Fragment {
      */
     private static final int ACTION_STORE_POI_WITH_CONTEXT_REQUEST_CODE = 0;
 
-    private Element osmElement;
+    private Poi mPoi;
+    private PoiRecommenderServiceInvoker mServiceInvoker;
+    private RatingProvider mRatingProvider;
 
     public static PoiFragment newInstance(Element poiElement) {
         PoiFragment poiFragment = new PoiFragment();
@@ -45,38 +46,50 @@ public class PoiFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        osmElement = new Serializer<>(Element.class)
+        Element poiElement = new Serializer<>(Element.class)
                 .deserialize(getArguments().getString(ARGUMENT_POI_ELEMENT));
-        Preconditions.checkNotNull(osmElement, "PoiFragment cannot be created without Element.");
+        Preconditions.checkNotNull(poiElement, "PoiFragment cannot be created without Element.");
+        mPoi = OsmPoi.fromOsmElement(poiElement);
+        mServiceInvoker = new PoiRecommenderServiceInvoker(
+                getActivity().getApplicationContext(),
+                ACTION_STORE_POI_WITH_CONTEXT_REQUEST_CODE);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.poi_fragment, container, false);
-        TextView poiNameText = (TextView) view.findViewById(R.id.poi_name);
-        TextView poiLatText = (TextView) view.findViewById(R.id.poi_lat);
-        TextView poiLonText = (TextView) view.findViewById(R.id.poi_lon);
-        Button poiSelectButton = (Button) view.findViewById(R.id.poi_select);
-        poiNameText.setText(osmElement.getTags().get(Element.ELEMENT_NAME_TAG));
-        poiLatText.setText(Double.toString(osmElement.getLat()));
-        poiLonText.setText(Double.toString(osmElement.getLon()));
-        poiSelectButton.setOnClickListener(new View.OnClickListener() {
+
+        TextView poiNameTextView = (TextView) view.findViewById(R.id.poi_name);
+        TextView poiLatitudeTextView = (TextView) view.findViewById(R.id.poi_lat);
+        TextView poiLongitudeTextView = (TextView) view.findViewById(R.id.poi_lon);
+        RatingBar poiRatingBar = (RatingBar) view.findViewById(R.id.rating_bar);
+        Button checkInPoiButton = (Button) view.findViewById(R.id.poi_select);
+
+        poiNameTextView.setText(mPoi.getName());
+        Location poiLocation = mPoi.getLocation();
+        poiLatitudeTextView.setText(Double.toString(poiLocation.getLatitude()));
+        poiLongitudeTextView.setText(Double.toString(poiLocation.getLongitude()));
+        mRatingProvider = new RatingProvider(poiRatingBar, getResources().getInteger(R.integer.poi_rating_stars_count));
+        checkInPoiButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent saveContextIntent = new Intent(PoiRecommenderService.ACTION_STORE_POI_WITH_CONTEXT);
-                saveContextIntent.putExtra(PoiRecommenderService.POI_EXTRA, new Serializer<>(Element.class).serialize(osmElement));
-                PendingIntent pendingIntent = PendingIntent.getService(
-                        getActivity().getApplicationContext(),
-                        ACTION_STORE_POI_WITH_CONTEXT_REQUEST_CODE,
-                        saveContextIntent,
-                        PendingIntent.FLAG_UPDATE_CURRENT);
-                try {
-                    pendingIntent.send();
-                } catch (PendingIntent.CanceledException e) {
-                    Log.e(TAG, "Sending pending intent failed...", e);
-                }
+                mServiceInvoker.storeAndRatePoiWithContext(mPoi.getElement(), mRatingProvider.getRating());
             }
         });
         return view;
+    }
+
+    private class RatingProvider {
+        private final RatingBar ratingBar;
+        private final int ratingBarStarsCount;
+
+        private RatingProvider(RatingBar ratingBar, int ratingBarStarsCount) {
+            this.ratingBar = ratingBar;
+            this.ratingBarStarsCount = ratingBarStarsCount;
+        }
+
+        public double getRating() {
+            return (ratingBar.getRating() - 1) / (ratingBarStarsCount - 1);
+        }
     }
 }
