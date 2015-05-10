57c8c05c0b58a493584c07889774dfe92746025c
BamBalooon <k.bb@wp.pl>
2015-05-10 16:32:07 +0200

    RF# added ProgressBar and CursorLoader to PoiFragment

--- Application/src/main/java/pl/edu/agh/eis/poirecommender/application/poi/PoiFragment.java
+++ Application/src/main/java/pl/edu/agh/eis/poirecommender/application/poi/PoiFragment.java
@@ -1,15 +1,21 @@
 package pl.edu.agh.eis.poirecommender.application.poi;
 
+import android.database.Cursor;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.RatingBar;
 import android.widget.TextView;
 import com.aware.poirecommender.openstreetmap.model.response.Element;
+import com.aware.poirecommender.provider.PoiRecommenderContract;
 import com.aware.poirecommender.transform.Serializer;
 import com.google.common.base.Preconditions;
 import pl.edu.agh.eis.poirecommender.R;
@@ -22,7 +28,7 @@ import pl.edu.agh.eis.poirecommender.pois.model.Poi;
  * Date: 2015-05-03
  * Created by BamBalooon
  */
-public class PoiFragment extends Fragment {
+public class PoiFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
     private static final String ARGUMENT_POI_ELEMENT = "ARGUMENT_POI_ELEMENT";
     /**
      * TODO: move request code constant to specific class with global request codes for pending intents
@@ -30,10 +36,12 @@ public class PoiFragment extends Fragment {
      * According to: http://codetheory.in/android-pending-intents/
      */
     private static final int ACTION_STORE_POI_WITH_CONTEXT_REQUEST_CODE = 0;
+    private static final int POI_RATING_LOADER = 0;
 
     private Poi mPoi;
     private PoiRecommenderServiceInvoker mServiceInvoker;
-    private RatingProvider mRatingProvider;
+    private ProgressBar mProgressBar;
+    private RatingBarDecorator mRatingBarDecorator;
     private Button mCheckInPoiButton;
 
     public static PoiFragment newInstance(Element poiElement) {
@@ -63,8 +71,11 @@ public class PoiFragment extends Fragment {
         TextView poiNameTextView = (TextView) view.findViewById(R.id.poi_name);
         TextView poiLatitudeTextView = (TextView) view.findViewById(R.id.poi_lat);
         TextView poiLongitudeTextView = (TextView) view.findViewById(R.id.poi_lon);
+        mProgressBar = (ProgressBar) view.findViewById(R.id.progress_bar);
         RatingBar poiRatingBar = (RatingBar) view.findViewById(R.id.rating_bar);
         mCheckInPoiButton = (Button) view.findViewById(R.id.poi_select);
+
+        getActivity().getSupportLoaderManager().restartLoader(POI_RATING_LOADER, null, this);
 
         poiNameTextView.setText(mPoi.getName());
         Location poiLocation = mPoi.getLocation();
@@ -76,28 +87,76 @@ public class PoiFragment extends Fragment {
                 mCheckInPoiButton.setEnabled(true);
             }
         });
-        mRatingProvider = new RatingProvider(poiRatingBar, getResources().getInteger(R.integer.poi_rating_stars_count));
+        mRatingBarDecorator = new RatingBarDecorator(poiRatingBar);
         mCheckInPoiButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mServiceInvoker.storeAndRatePoiWithContext(mPoi.getElement(), mRatingProvider.getRating());
+                mServiceInvoker.storeAndRatePoiWithContext(mPoi.getElement(), mRatingBarDecorator.getRating());
                 mCheckInPoiButton.setEnabled(false);
+                mProgressBar.setVisibility(View.VISIBLE);
+                mRatingBarDecorator.disable();
             }
         });
         return view;
     }
 
-    private class RatingProvider {
-        private final RatingBar ratingBar;
-        private final int ratingBarStarsCount;
+    @Override
+    public Loader<Cursor> onCreateLoader(int loaderId, Bundle args) {
+        switch (loaderId) {
+            case POI_RATING_LOADER:
+                return new CursorLoader(
+                        getActivity(),
+                        PoiRecommenderContract.PoisRating.CONTENT_URI,
+                        new String[]{PoiRecommenderContract.PoisRating.POI_RATING},
+                        PoiRecommenderContract.PoisRating.POI_ID + "=?",
+                        new String[]{Long.toString(mPoi.getElement().getId())},
+                        PoiRecommenderContract.PoisRating._ID + " DESC");
+            default:
+                return null;
+        }
+    }
 
-        private RatingProvider(RatingBar ratingBar, int ratingBarStarsCount) {
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor poiRatingCursor) {
+        if (poiRatingCursor != null && poiRatingCursor.moveToFirst()) {
+            int poiRatingColumnIndex = poiRatingCursor.getColumnIndex(PoiRecommenderContract.PoisRating.POI_RATING);
+            double poiRating = poiRatingCursor.getDouble(poiRatingColumnIndex);
+            mRatingBarDecorator.setRating(poiRating);
+        }
+        mProgressBar.setVisibility(View.INVISIBLE);
+        mRatingBarDecorator.enable();
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+    }
+
+    private class RatingBarDecorator {
+        private static final float RATING_BAR_DISABLED_ALPHA = .2f;
+        private static final float RATING_BAR_ENABLED_ALPHA = 1f;
+        private final RatingBar ratingBar;
+
+        private RatingBarDecorator(RatingBar ratingBar) {
             this.ratingBar = ratingBar;
-            this.ratingBarStarsCount = ratingBarStarsCount;
         }
 
         public double getRating() {
-            return (ratingBar.getRating() - 1) / (ratingBarStarsCount - 1);
+            return (ratingBar.getRating() - 1) / (ratingBar.getNumStars() - 1);
+        }
+
+        public void setRating(double rating) {
+            ratingBar.setRating((float) rating * (ratingBar.getNumStars() - 1) + 1);
+        }
+
+        public void disable() {
+            ratingBar.setIsIndicator(true);
+            ratingBar.setAlpha(RATING_BAR_DISABLED_ALPHA);
+        }
+
+        public void enable() {
+            ratingBar.setVisibility(View.VISIBLE);
+            ratingBar.setIsIndicator(false);
+            ratingBar.setAlpha(RATING_BAR_ENABLED_ALPHA);
         }
     }
 }
