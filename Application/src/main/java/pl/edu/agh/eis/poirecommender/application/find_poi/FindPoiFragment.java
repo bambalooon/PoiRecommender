5d2739f2daebe33077d9b7dec5ff9dc7b58661f2
BamBalooon <k.bb@wp.pl>
2015-07-05 11:55:10 +0200

    SC# added menu to find POI fragment

--- Application/src/main/java/pl/edu/agh/eis/poirecommender/application/find_poi/FindPoiFragment.java
+++ Application/src/main/java/pl/edu/agh/eis/poirecommender/application/find_poi/FindPoiFragment.java
@@ -1,6 +1,11 @@
 package pl.edu.agh.eis.poirecommender.application.find_poi;
 
+import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import pl.edu.agh.eis.poirecommender.R;
 
 /**
  * Name: FindPoiFragment
@@ -9,4 +14,24 @@ import android.support.v4.app.ListFragment;
  * Created by BamBalooon
  */
 public class FindPoiFragment extends ListFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.find_poi, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_find_poi:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
