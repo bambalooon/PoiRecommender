a0ae311fe1c2fc296a7c508d26e00154baad300c
Krzysztof Balon <k.bb@wp.pl>
2015-09-04 22:59:24 +0200

    RF# added TO_STRING constant function to QueryPart interface

--- Application/src/main/java/pl/edu/agh/eis/poirecommender/openstreetmap/model/request/QueryPart.java
+++ Application/src/main/java/pl/edu/agh/eis/poirecommender/openstreetmap/model/request/QueryPart.java
@@ -1,5 +1,14 @@
 package pl.edu.agh.eis.poirecommender.openstreetmap.model.request;
 
+import com.google.common.base.Function;
+
 public interface QueryPart {
     String createQueryPart();
+
+    Function<QueryPart, String> TO_STRING = new Function<QueryPart, String>() {
+        @Override
+        public String apply(QueryPart queryPart) {
+            return queryPart.createQueryPart();
+        }
+    };
 }
